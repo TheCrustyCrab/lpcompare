d300c8e0666a2b116b0dda0ac4355dbbdf63363c
TheCrustyCrab <155241108+TheCrustyCrab@users.noreply.github.com>
2024-08-05 09:52:04 +0200

    Initial app to solve .lp and .mps files with HiGHS and lpsolve

---
+++ example/test.mps
@@ -0,0 +1,21 @@
+NAME          TEST
+OBJSENSE
+  MAX
+ROWS
+ N  Obj     
+ L  r0      
+ L  r1      
+COLUMNS
+    c0        Obj       10
+    c0        r0        1
+    c0        r1        1
+    c1        Obj       11
+    c1        r0        2
+    c1        r1        1
+RHS
+    RHS_V     r0        5
+    RHS_V     r1        3
+BOUNDS
+ FR BOUND     c0      
+ FR BOUND     c1      
+ENDATA
---
+++ src/main.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include "Highs.h"
+#include "lp_lib.h"
+
+using namespace std;
+
+enum class Solver {
+    Highs,
+    Lpsolve
+};
+
+enum class FileType {
+    Lp,
+    Mps
+};
+
+struct LpCompareOpts {
+    Solver solver;
+    FileType fileType;
+    string inputFile;
+};
+
+bool parseArgs(int argc, char** argv, LpCompareOpts& opts) {
+    if (argc != 3) {
+        cout << "Invalid argument count, expected 2." << endl;
+        return false;
+    }
+
+    Solver solver;
+    char* solverId = argv[1];
+    if (*solverId == '0') {
+        solver = Solver::Highs;
+    } else if (*solverId == '1') {
+        solver = Solver::Lpsolve;
+    } else {
+        cout << "Invalid solver " << solverId << endl;
+        return false;
+    }
+
+    string inputFile = argv[2];
+    FileType fileType;
+    int inputFileLen = inputFile.length();
+    if (inputFileLen < 4) {
+        cout << "Invalid input file (type)" << endl;
+        return false;
+    }
+    if (inputFile.compare(inputFileLen - 3, 3, ".lp") == 0) {
+        fileType = FileType::Lp;
+    } else if (inputFile.compare(inputFileLen - 4, 4, ".mps") == 0) {
+        fileType = FileType::Mps;
+    } else {
+        cout << "Invalid input file (type)" << endl;
+        return false;
+    }
+
+    opts.solver = solver;
+    opts.fileType = fileType;
+    opts.inputFile = inputFile;
+    return true;
+}
+
+int main(int argc, char** argv) {
+    LpCompareOpts opts;
+    if (!parseArgs(argc, argv, opts)) {
+        cout << "failed to parse" << endl;
+        return -1;
+    }
+    
+    if (opts.solver == Solver::Highs) {
+        Highs highs;
+        highs.readModel(opts.inputFile);
+        HighsStatus status = highs.run();
+        return (int)status;
+    } else { // lpsolve
+        FILE* file = fopen(opts.inputFile.c_str(), "r");
+        if (!file) {
+            cout << "Input file not found" << endl;
+            return -1;
+        }
+
+        lprec* lp;
+        if (opts.fileType == FileType::Lp) {
+            lp = read_lp(file, 0, "lpcompare");
+        } else { // Mps
+            lp = read_mps(file, NORMAL | MPS_FREE);
+        }        
+        if (!lp) {
+            return -1;
+        }
+
+        int status = solve(lp);
+        return status;
+    }
+
+    return 0;
+}
